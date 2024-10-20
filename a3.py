Fixing up fuel mass formula and adding more clarity to print statements

a3.py
@@ -31,7 +31,7 @@ def main() -> None:
     inc_ang_org, raan_org = -33.42, 144                 # Inclination angle and Right Angle of Ascending Node of original orbit
     period_org = form.period(r_org, mu)                 # Period of original orbit
     h_org = np.sqrt(r_org * mu)                         # Angular momentum of original orbit
-    m0 = 270                                            # Satellite dry mass
+    m0 = 40                                             # Satellite dry mass (kg)
 
     # Extract Orbital parameters of each target satellite
     print("\033[4m" + "Hubble Space Telescope:" + "\033[0m")
@@ -73,6 +73,7 @@ def main() -> None:
     mission_delta_v = lc.sort_orb_efficiency(current_orbit, orbits,
                     omega_e, points_sim, m0, isp, earth_rad, mu)
 
+    form.change_in_mass(mission_delta_v, m0, isp)
     print(f"Total most efficient mission delta-v (km/s):     {mission_delta_v:.3f}")
 
 if __name__ == '__main__':

a3_formulae.py
@@ -48,10 +48,11 @@ def delta_plane(angular_momentum : float, radius : float, ang_1 : float, ang_2 :
 
     return delta_v
 
-def change_in_mass(delta_v, m0, specific_impulse, gravity = 9.81) -> float:
+def change_in_mass(delta_v, mf, specific_impulse, gravity = 9.81) -> float:
     delta_vms = delta_v * 1000
-    dm = m0 * (1-np.e**(-delta_vms/(specific_impulse*gravity)))
-    print(f"Fuel mass required (Isp = {specific_impulse}):              {dm:.3f}kg")
+    m0 = np.e**(delta_vms/(specific_impulse*gravity)) * mf
+    dm = m0 - mf
+    print(f"Fuel mass required (Isp = {specific_impulse}):              {m0:.3f}kg")
     return dm
 
 def total_time(period_chase : float, period_mid : float,